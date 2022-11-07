19933b638a153fe369b187e66bbcad85c5dc50cc
zlig <gdevillard@gmail.com>
2022-11-07 16:28:08 +0000

    Adds creation script

--- info-db/01_create_tables.sql
+++ info-db/01_create_tables.sql
@@ -1,6 +1,17 @@
+-- Dropping tables
+DROP TABLE IF EXISTS content;
+DROP TABLE IF EXISTS version;
+
+-- Creation of version table
+CREATE TABLE content (
+	content_id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR (255),
+        last_update DATE
+);
+
 -- Creation of version table
 CREATE TABLE IF NOT EXISTS version (
   version SMALLINT DEFAULT 0 NOT NULL
 );
-
-
+INSERT INTO version(version) VALUES ('1');
