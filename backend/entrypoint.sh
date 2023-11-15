53db910b044247e372c6f55463a04faea3b35980
zlig <gdevillard@gmail.com>
2023-11-15 20:39:10 +0000

    Blank out to allow running

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,21 +3,21 @@ set +x
 set -e
 
 # Variables
-export AZ_USER=$(echo $USER | base64 -d)
-export AZ_PASS=$(echo $PASS | base64 -d)
-export AZ_TENANT=$(echo $TENANT | base64 -d)
-export AZ_SUBID=$(echo $SUBID | base64 -d)
+# export AZ_USER=$(echo $USER | base64 -d)
+# export AZ_PASS=$(echo $PASS | base64 -d)
+# export AZ_TENANT=$(echo $TENANT | base64 -d)
+# export AZ_SUBID=$(echo $SUBID | base64 -d)
 export PG_PASS=$(echo $PG_PASS | base64 -d)
 
 # Login
-az login --service-principal -u "$AZ_USER" -p "$AZ_PASS" --tenant "$AZ_TENANT"
-az account set -s "$AZ_SUBID"
-az aks get-credentials -g "$AKS_RG" -n "$AKS_NAME" --admin
+# az login --service-principal -u "$AZ_USER" -p "$AZ_PASS" --tenant "$AZ_TENANT"
+# az account set -s "$AZ_SUBID"
+# az aks get-credentials -g "$AKS_RG" -n "$AKS_NAME" --admin
 
 # Processing
 while true; do
   ## VMS
-  az version
+  # az version
 
   ## LOOP
   sleep 6m
