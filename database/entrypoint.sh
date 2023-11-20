063920b691a419f6c855e3626a74134975d5842e
zlig <gdevillard@gmail.com>
2023-11-20 19:37:30 +0000

    Fix docker entrypoint script

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -5,81 +5,66 @@
 set -x
 set -e
 
-whoami
-
-# # Create a directory to store the schema versions
-# mkdir -p /var/lib/postgresql/schema
-
-# # Get the current schema version
-# if [ -f /var/lib/postgresql/schema/version.txt ]; then
-#   CURRENT_VERSION=$(cat /var/lib/postgresql/schema/version.txt)
-# else
-#   CURRENT_VERSION=0
-# fi
-
-# # Loop through the SQL scripts and apply them if they are newer than the current version
-# for SCRIPT in $(ls /scripts/*.sql); do
-#   SCRIPT_NAME=$(basename $SCRIPT)
-#   SCRIPT_VERSION=$(echo $SCRIPT_NAME | cut -d'_' -f1)
-
-#   if [ $SCRIPT_VERSION -gt $CURRENT_VERSION ]; then
-#     echo "Applying $SCRIPT_NAME"
-#     psql -U postgres -d mydb -f $SCRIPT
-#     echo $SCRIPT_VERSION > /var/lib/postgresql/schema/version.txt
-#   fi
-# done
-
-# # Executes all schema updates above current version
-# echo "Database exists, updating database schema..."
-# for SQL_FILE in \`ls /scripts/schema_update_*.sql\`;
-# do
-#   UPDATE_VERSION=\`echo \$SQL_FILE | sed "s|/scripts/schema_update_||" | sed "s|.sql||"\`
-
-#   if [[ "\$UPDATE_VERSION" -gt "\$DBVERSION" ]]; then
-#     echo "Applying schema updates from file \$SQL_FILE to update database schema to version \$UPDATE_VERSION"
-#     psql -U postgres -d mydb -f $SCRIPT
-#   fi
-# done
-
-
-set +x
-
-# Start the PostGreSQL server
-exec /usr/local/bin/docker-entrypoint.sh "$@"
-
-# DEBUG
-ls -l 
-
 # Define variables
-SCHEMA_VERSION_TABLE="schema_version"
-SCRIPTS_DIR="/scripts"
-# DATABASE_NAME="mydatabase"
-# POSTGRES_USER="myuser"                 
-# DATABASE_PASSWORD="mypassword"
+SCHEMA_VERSION_TABLE="version"
+SCRIPTS_DIR="/docker-entrypoint-initdb.d"
 
-# DEBUG
-ls -lRt $SCRIPTS_DIR
+# # DEBUG
+# echo "Displaying debug information.."
+# whoami
+# pwd
+# ls -l $SCRIPTS_DIR
 
 
-# Check if schema version table exists
-if ! psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\d $SCHEMA_VERSION_TABLE" >/dev/null 2>&1; then
-  # Create schema version table if it doesn't exist
-  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE TABLE $SCHEMA_VERSION_TABLE (version INTEGER NOT NULL); INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES (0);"
+# Versions the database schema (if it exists)
+echo "Versioning database schema.."
+
+# look specifically for PG_VERSION, as it is expected in the DB dir
+declare -g DATABASE_ALREADY_EXISTS
+if [ -s "$PGDATA/PG_VERSION" ]; then
+  DATABASE_ALREADY_EXISTS='true'
 fi
 
-# Get current schema version
-CURRENT_VERSION=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -c "SELECT version FROM $SCHEMA_VERSION_TABLE")
+# Applies update script if database already exists
+if [ ! -z "$DATABASE_ALREADY_EXISTS" ]; then
 
-# Apply SQL scripts
-for script in $(ls $SCRIPTS_DIR/*.sql | sort -V); do
-  VERSION=$(echo $script | sed -E 's/.*\/([0-9]+)\-.*/\1/')
-  if [ $VERSION -gt $CURRENT_VERSION ]; then
-    echo "Applying script $script"
-    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f $script
-    # Update schema version
-    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "UPDATE $SCHEMA_VERSION_TABLE SET version=$VERSION"
-  fi
-done
+  # Reuse PostgreSQL script as library of functions
+  source /usr/local/bin/docker-entrypoint.sh
+  docker_setup_env
 
-# Bring back entrypoint in foreground
-fg %1
+  # Temporarily start PostgreSQL server
+  su -l $POSTGRES_USER -c "pg_ctl start -s -l /dev/null -D $PGDATA"
+
+  # Get current schema version
+  CURRENT_VERSION=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -c "SELECT version FROM $SCHEMA_VERSION_TABLE")
+
+  # Apply SQL scripts
+  for script in $(ls $SCRIPTS_DIR/*.sql | sort -V); do
+
+    # Get script version
+    SCRIPT_VERSION=$(echo $script | sed -E 's/.*\/([0-9]+)[\_\-].*/\1/')
+
+    # Applying SQL script (if applicable)
+    echo "Checking if script version $SCRIPT_VERSION should be applied on current database schema version $CURRENT_VERSION"
+    if [ $SCRIPT_VERSION -gt $CURRENT_VERSION ]; then
+      echo "Applying script $script"
+      docker_process_init_files $script
+
+      # Update schema version
+      psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "UPDATE $SCHEMA_VERSION_TABLE SET version=$SCRIPT_VERSION"
+    fi
+  done
+
+  # Stop temporary instance of PostgreSQL server
+  su -l $POSTGRES_USER -c "pg_ctl stop -s -l /dev/null -D $PGDATA"
+  unset DATABASE_ALREADY_EXISTS
+
+  # Process completed
+  echo 'PostgreSQL schema update process completed; ready for start up.'
+
+fi
+
+
+# Start the PostGreSQL server
+echo "Starting postgres.."
+exec /usr/local/bin/docker-entrypoint.sh "$@"
