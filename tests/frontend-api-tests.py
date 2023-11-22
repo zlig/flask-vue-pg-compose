a8077de493e16ca2d4aa0db5ae0aca2e58e690b5
zlig <gdevillard@gmail.com>
2023-11-22 19:34:24 +0000

    Fixed test and added usage

--- tests/frontend-api-tests.py
+++ tests/frontend-api-tests.py
@@ -1,27 +1,42 @@
-# from locust import HttpUser, task
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Frontend API validation and load testing
+#
+# Usage:
+#   
+#   pip install -r requirements.txt
+#
+#   locust --host=http://0.0.0.0:8081 -f frontend-api-tests.py
+#   firefox http://0.0.0.0:8089/
+#
+#   (headless without Web UI)
+#   locust --headless --users 10 --spawn-rate 1 --host=http://0.0.0.0:8081 -f frontend-api-tests.py
 
-# class HelloWorldUser(HttpUser):
-#     @task
-#     def hello_world(self):
-#         self.client.get("/hi")
-#         self.client.get("/hi/flaskie")
+from locust import HttpUser, task
 
-
-from locust import HttpUser, TaskSet, task
-
-class FrontendApi(TaskSet):
-
+class HelloWorldUser(HttpUser):
     @task
-    def get_tests(self):
+    def hello_world(self):
         self.client.get("/hi")
         self.client.get("/hi/flaskie")
+
+
+# from locust import HttpUser, TaskSet, task
+
+# class FrontendApi(TaskSet):
+
+#     @task
+#     def get_tests(self):
+#         self.client.get("/hi")
+#         self.client.get("/hi/flaskie")
         
-    @task
-    def put_tests(self):
-        self.client.post("/tests", {
-						  "name": "load testing",
-						  "description": "checking if a software can handle the expected load"
-						})
+#     @task
+#     def put_tests(self):
+#         self.client.post("/tests", {
+# 						  "name": "load testing",
+# 						  "description": "checking if a software can handle the expected load"
+# 						})
         
-class WebsiteUser(HttpUser):
-    task_set = FrontendApi
+# class WebsiteUser(HttpUser):
+#     task_set = FrontendApi
