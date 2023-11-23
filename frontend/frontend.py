eb50d598c7e27b12b87978caac06f1c8c12c0f76
zlig <gdevillard@gmail.com>
2023-11-23 19:37:04 +0000

    Amends following change schema

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -42,13 +42,13 @@ redis = Redis(host='info-redis', port=6379)
 
 # Models
 class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     age = db.Column(db.Integer)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    bio = db.Column(db.Text)
+    biography = db.Column(db.Text)
 
     def __repr__(self):
         return f'<Account {self.firstname} {self.lastname} {self.email}>'
