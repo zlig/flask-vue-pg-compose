47ba0454eb02b8c332b96e7a4dd17921e0d89efa
zlig <gdevillard@gmail.com>
2023-11-23 19:35:00 +0000

    Change schema

--- database/sql/02_create_account_table.sql
+++ database/sql/02_create_account_table.sql
@@ -1,17 +1,17 @@
 -- NOTE:
---   This is the users' account table, but user is a reserved word in PostgreSQL..
---   It needs to be placed in double-quotes, or use alternative table names (such as account, user_account, client, customer)
+--   This is the users accounts table, but 'user' is a reserved word in PostgreSQL..
+--   It needs to be placed in double-quotes, i.e. "user", or use alternative table names (such as account, user_account, client, customer)
 
 -- Dropping table
 DROP TABLE IF EXISTS account;
 
 -- Creation of account table
 CREATE TABLE account (
-	user_id SERIAL PRIMARY KEY,
+	account_id SERIAL PRIMARY KEY,
 	firstname VARCHAR(100) NOT NULL,
 	lastname VARCHAR(100) NOT NULL,
 	email VARCHAR(80) NOT NULL,
-	biography VARCHAR (255),
+	biography TEXT,
     age SMALLINT NOT NULL,
     created_at DATE,
     last_updated DATE
