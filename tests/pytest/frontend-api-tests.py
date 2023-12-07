a36e789fc4703cebfafd42fb45eaa02c2c4f0bfd
zlig <gdevillard@gmail.com>
2023-12-07 19:52:36 +0000

    Experimenting

--- tests/pytest/frontend-api-tests.py
+++ tests/pytest/frontend-api-tests.py
@@ -1,12 +1,29 @@
 import requests
 
-url = 'http://127.0.0.1:8081/hi'
+host = 'http://127.0.0.1:8081'
 
-# Make a GET request to the API endpoint
-response = requests.get(url)
+# # Make a GET request to the API endpoint
+# response = requests.get(url)
 
-if response.status_code == 200:
-    print(response.json())
-else:
-    print('Error: {0}'.format(response.status_code)) 
-      
+# if response.status_code == 200:
+#     print(response.json())
+# else:
+#     print('Error: {0}'.format(response.status_code)) 
+
+
+import requests
+import pytest
+
+
+def test_1_get():
+    url = host+"/ping"
+    res = requests.get(url)
+    print("RESPONSE:", res.json())
+    assert res.status_code == 200
+
+def test_2_put():
+    url = host+"/accounts"
+    data = {"name":"string"}
+    res = requests.post(url, json=data)
+    print("RESPONSE:", res.json())
+    assert res.status_code == 200
