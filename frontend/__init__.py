acc55406efb57bfa15bf543a75bb1c38b87bb336
zlig <gdevillard@gmail.com>
2024-02-13 22:11:14 +0000

    Add comments

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,5 +1,6 @@
-#  Defines directory as package, to allow importing files
+# Defines directory as package, to allow importing files
 import os
 import sys
 
+# Refers to paths regardless of the script location. 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
