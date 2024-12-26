396f09f18a0dece390cddaf5dba489c8f8465442
zlig <gdevillard@gmail.com>
2024-12-26 15:53:58 +0000

    Amend schema

---
+++ database/sql/04_update_article_table.sql
@@ -0,0 +1,19 @@
+-- NOTE: PostgreSQL does support adding a column at a specific location
+
+-- Dropping table
+DROP TABLE IF EXISTS article;
+
+-- Creation of article table with new title column
+CREATE TABLE article (
+	article_id SERIAL PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR (255),
+    created_at TIMESTAMPTZ DEFAULT Now(),
+    last_updated TIMESTAMPTZ DEFAULT Now(),
+	account_id SERIAL,
+	FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+-- Update database schema version
+UPDATE version SET version=4;
