d8ba617cc2e3280f9d2e208353c3a9f606349b50
zlig <gdevillard@gmail.com>
2024-12-28 10:50:05 +0000

    Add main content column

---
+++ database/sql/05_update_article_table .sql
@@ -0,0 +1,17 @@
+-- NOTE: PostgreSQL dhas restriction on columns to not use
+--	     https://www.postgresql.org/docs/current/sql-keywords-appendix.html
+
+-- Creation of article table with title and content columns
+CREATE TABLE article (
+	article_id SERIAL PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	description VARCHAR (255),
+	main TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT Now(),
+    last_updated TIMESTAMPTZ DEFAULT Now(),
+	account_id SERIAL,
+	FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+-- Update database schema version
+UPDATE version SET version=5;
